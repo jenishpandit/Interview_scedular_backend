f01baec1c32ab17207f803798ff55fb22a974396
pratham <prathamhathiwala9@gmail.com>
2024-06-22 18:02:13 +0530

    - Fix dotenv file issue

--- constants/constants.js
+++ constants/constants.js
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 export const MONGO_DB_URL = process.env.MONGO_DB_URL || "mongodb://localhost:27017/interview_scheduler";
 export const PORT = process.env.PORT || 4000;
-export const SECRET_KEY = process.env.SECRET_KEY || "asdasdasdsad";
+export const SECRET_KEY = process.env.SECRET_KEY || "interview_scheduler";
